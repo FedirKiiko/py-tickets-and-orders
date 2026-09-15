501e2abdc90043bdd86ae916d2b289ef1870f73e
Fedir Kiiko <fedir1357@gmail.com>
2026-09-15 23:37:27 +0300

    Solution

---
+++ services/order.py
@@ -0,0 +1,42 @@
+import datetime
+
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, Ticket, MovieSession
+
+
+@transaction.atomic
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: datetime.datetime | None = None
+) -> Order:
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(
+        user=user,
+    )
+    if date:
+        order.created_at = date
+        order.save()
+    for ticket in tickets:
+        Ticket.objects.create(
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session=MovieSession.objects.get(
+                pk=ticket["movie_session"]
+            ),
+            order=order
+        )
+
+    return order
+
+
+def get_orders(
+        username: str | None = None
+) -> QuerySet[Order]:
+    orders = Order.objects.all()
+    if username:
+        orders = orders.filter(user__username=username)
+    return orders
